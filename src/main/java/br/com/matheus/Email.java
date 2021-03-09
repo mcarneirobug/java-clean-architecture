ea5dd0b45e1031af8a036b8543247335dd4e582a
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-03-09 14:52:14 -0300

    feat: Created entity and VO

---
+++ src/main/java/br/com/matheus/Email.java
@@ -0,0 +1,18 @@
+package br.com.matheus;
+
+public class Email {
+
+    private String nameAddress;
+
+    public Email(String nameAddress) {
+        this.setNameAddress(nameAddress);
+    }
+
+    public void setNameAddress(String nameAddress) {
+        if(nameAddress == null ||
+                !nameAddress.matches("^([a-zA-Z0-9_\\-.]+)@([a-zA-Z0-9_\\-]+)\\.([a-zA-Z]{2,})$")) {
+            throw new IllegalArgumentException("Invalid e-mail exception!");
+        }
+        this.nameAddress = nameAddress;
+    }
+}
