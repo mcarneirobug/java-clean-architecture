f18950a5640bdc31de2f37822b3f71e83d4f5fce
Matheus Santos <mcarneiro@sga.pucminas.br>
2021-03-09 16:45:39 -0300

    feat: Added phone to Student

--- src/main/java/br/com/matheus/Student.java
+++ src/main/java/br/com/matheus/Student.java
@@ -1,8 +1,19 @@
 package br.com.matheus;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
 
     private String name;
+
     private CPF cpf;
+
     private Email email;
+
+    private List<Phone> phones = new ArrayList<>();
+
+    public void addPhone(String ddd, String number) {
+        this.phones.add(new Phone(ddd, number));
+    }
 }
